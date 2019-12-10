815b723c3cbbe9f38540532f78b2cf19cadaed13
Techno-Hwizrdry <alex.mardigian@gmail.com>
2019-12-10 17:24:20 -0500

    first commit

---
+++ Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,26 @@
+﻿/*
+  Author:  Alexan Mardigian
+  alex.mardigian@gmail.com
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour {
+	public float despawnHeight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < despawnHeight) {
+			LevelGenerator.level = -1;  // Reset the level counter that is displayed to the user.
+			SceneManager.LoadScene("GameOver");
+		}
+	}
+}
---
+++ Assets/Scripts/LoadTitleSceneOnInput.cs
@@ -0,0 +1,27 @@
+﻿/*
+  Author:  Alexan Mardigian
+  alex.mardigian@gmail.com
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTitleSceneOnInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		/* Destroy the WhisperSource here, on the GameOver scene,
+		   before the WhisperSource has a chance to overlap with the Title scene music.
+		*/ 
+		Destroy(GameObject.Find("WhisperSource"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Submit") == 1) {
+			SceneManager.LoadScene("Title");
+		}
+	}
+}
